80e95a132402c1ac4a8a519c4f65de5ea87183cb
hrissolo <hrissolo@gmail.com>
2021-02-04 15:44:43 -0600

    denied table listing

--- src/components/application/ApplicantTabs.js
+++ src/components/application/ApplicantTabs.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
 import { ApplicantList } from "./ApplicantList"
+import { DeniedList } from "./DeniedList"
 
 const panes = [
   { menuItem: 'Applicants', render: () => <Tab.Pane> <ApplicantList/> </Tab.Pane> },
   { menuItem: 'Accepted', render: () => <Tab.Pane>Table of accepted applicants.. no intake date yet.</Tab.Pane> },
-  { menuItem: 'Denied', render: () => <Tab.Pane>Tab 3 Content</Tab.Pane> },
+  { menuItem: 'Denied', render: () => <Tab.Pane>  <DeniedList/> </Tab.Pane> },
 ]
 
 export const ApplicantTabs = () => <Tab panes={panes}  />
---
+++ src/components/application/DeniedList.js
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect } from "react"
+import { ResidentContext} from "../residents/ResidentProvider"
+import { DeniedCard } from "./DeniedCard"
+import {useHistory} from "react-router-dom"
+import { Table } from 'semantic-ui-react'
+
+
+export const DeniedList = () => {
+    const { residents, getResidents } = useContext(ResidentContext)
+    const history = useHistory()
+    
+	//useEffect - reach out to the world for something
+    useEffect(() => {
+		getResidents()
+		
+    }, [])
+
+    const deniedApplicants = ((rezzy) => 
+      {
+        if (rezzy.discharge_date === null && rezzy.intake_date === null) {
+        return ( 
+        <DeniedCard key={rezzy.id} residents={rezzy} /> ) } 
+    
+        else {
+          return null
+        }
+      })
+
+
+    return (
+      
+    <>
+    <div className="deniedListcontainer">
+      <h1 className="deniedTitle">Denied Applicant</h1>
+          
+      <div className="deniedContainer">
+      
+      <Table striped>
+        <Table.Header>
+        <Table.Row>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Applied Date</Table.HeaderCell>
+            <Table.HeaderCell>Notes</Table.HeaderCell>
+        </Table.Row>
+            </Table.Header>
+
+        <Table.Body>
+
+        
+        {
+        residents.map(residents => {
+          
+          return deniedApplicants(residents)
+          })
+        }
+
+        </Table.Body>
+        </Table>
+
+      </div>
+      </div>
+    </>
+  )
+}
+
+
+
